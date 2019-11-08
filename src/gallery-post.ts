85b7940f39a2cd822cfc6ad18d80f1776ef6f4c5
Kit Westneat <kit.westneat@gmail.com>
2019-11-07 19:11:05 -0500

    improve gallery post test

--- src/gallery-post.ts
+++ src/gallery-post.ts
@@ -1,7 +1,7 @@
 import * as webdriver from 'selenium-webdriver';
 import { assert } from 'chai';
-import { sleep, clickElement } from './utils';
-import { GALLERY_POST } from './constants';
+import { sleep, getNetworkEntries } from './utils';
+import { GALLERY_POST, GALLERY_FLOORBOARD_UNIT_NAME, AMZN_URL } from './constants';
 
 const GALLERY_BUTTON_TEXT = 'View Gallery';
 
@@ -13,11 +13,31 @@ describe('Gallery post tests', function() {
         await sleep(2000);
     });
     it.only('open gallery', async function() {
-        const openButton = await this.driver.findElements(
+        const driver = this.driver as webdriver.ThenableWebDriver;
+        const openButton = await driver.findElements(
             webdriver.By.xpath(`//div[text()='${GALLERY_BUTTON_TEXT}']`)
         );
 
-        clickElement(this.driver, openButton);
-        await sleep(10000);
+        const galleryAdDiv = await driver.findElements(
+            webdriver.By.css('.gallery-descriptions-wrap > .mrec-wrap > div')
+        );
+        assert(!await galleryAdDiv[0].isDisplayed(), 'Ad div is not visible before click');
+
+        const entries = await getNetworkEntries(this.driver);
+        const gallery_bids = entries.filter(({ name }: { name: string }) =>
+            name.startsWith(AMZN_URL) && name.includes(GALLERY_FLOORBOARD_UNIT_NAME)
+        );
+        assert(gallery_bids.length === 0, 'No gallery floorboard Amazon bids before opening');
+
+        await openButton[0].click();
+        await sleep(1000);
+
+        assert(await galleryAdDiv[0].isDisplayed(), 'Ad div is visible after click');
+
+        const post_entries = await getNetworkEntries(this.driver);
+        const post_gallery_bids = post_entries.filter(({ name }: { name: string }) =>
+            name.startsWith(AMZN_URL) && name.includes(GALLERY_FLOORBOARD_UNIT_NAME)
+        );
+        assert(post_gallery_bids.length !== 0, 'Gallery floorboard Amazon bids after opening');
     });
 });
