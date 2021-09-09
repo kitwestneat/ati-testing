877d969b4480a80fddb33d8fb6663e83850fc435
Kit Westneat <kit@pbh-network.com>
2021-09-09 13:58:52 -0400

    Refactor full width test

--- src/tools/full-width-screenshots.ts
+++ src/tools/full-width-screenshots.ts
@@ -3,9 +3,10 @@ import * as webdriver from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
 import { WINDOW_SIZES } from '../constants';
 import { sleep } from '../utils';
+import { send_email } from '../mail';
 
 const BASE_URL = 'https://mirror2.pbh-network.com/';
-const TEST_URLS = [
+const TEST_SLUGS = [
     'thylacine',
     'mary-church-terrell',
     'true-scary-stories',
@@ -30,32 +31,48 @@ function initChrome(): webdriver.ThenableWebDriver {
     return new webdriver.Builder().withCapabilities(capa).setChromeOptions(options).build();
 }
 
-async function saveScreenshot(driver: any, slug: string, size: string) {
-    const filename = `/tmp/${slug}.${size}.png`;
+function makeScreenshotFilename(slug: string, size: string): string {
+    return `/tmp/${slug}.${size}.png`;
+}
 
-    const data = await driver.takeScreenshot();
-    fs.writeFileSync(filename, data, 'base64');
-    console.log('write', filename);
+interface Rect {
+    width: number;
+    height: number;
+}
+
+async function getScreenshotList(driver: any, urlList: string[], size: Rect): Promise<string[]> {
+    const filenameList: string[] = [];
+
+    driver.manage().window().setRect(size);
+    for (const url of urlList) {
+        await driver.get(url);
+        await sleep(SLEEP_TIME);
+
+        const filename = makeScreenshotFilename(url, size.width + 'x' + size.height);
+        const data = await driver.takeScreenshot();
+        fs.writeFileSync(filename, data, 'base64');
+        filenameList.push(filename);
+    }
+
+    return filenameList;
 }
 
 async function main() {
     const driver = initChrome();
+    let filename_list: string[] = [];
+    const urlList = TEST_SLUGS.map((slug) => BASE_URL + slug);
 
     for (const windowSize of WINDOW_SIZES) {
-        driver.manage().window().setRect(windowSize);
-        for (const url of TEST_URLS) {
-            await driver.get(BASE_URL + url);
-            await sleep(SLEEP_TIME);
-
-            await saveScreenshot(driver, url, windowSize.width + 'x' + windowSize.height);
-        }
+        filename_list = filename_list.concat(await getScreenshotList(driver, urlList, windowSize));
         const fullSize = { width: windowSize.width, height: 2048 };
-        driver.manage().window().setRect(fullSize);
-        for (const url of TEST_URLS) {
-            await driver.get(BASE_URL + url);
-            await sleep(SLEEP_TIME);
-            await saveScreenshot(driver, url, fullSize.width + 'x' + fullSize.height);
-        }
+        filename_list = filename_list.concat(await getScreenshotList(driver, urlList, fullSize));
     }
+
+    send_email({
+        subject: 'ATI Full Width Screenshots',
+        body: 'Screenshots of full width headers attached',
+        attachments: filename_list.map((path) => ({ path })),
+        to: 'kit@pbh-network.com',
+    });
 }
 main();
